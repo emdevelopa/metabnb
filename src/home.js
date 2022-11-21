6f08cbe6b1231489838ca29f02b633c2a05519af
emdevelopa <gyimahemwurld@gmail.com>
2022-11-21 12:12:46 -0800

    new commit

--- src/home.js
+++ src/home.js
@@ -1,11 +1,15 @@
 import house from "./logo/metahomelogo.png";
 import window from "./logo/metawindow.png";
+import img1 from "./sec1_img/img1.png";
+import img2 from "./sec1_img/img2.png";
+import img3 from "./sec1_img/img3.png";
+import img4 from "./sec1_img/img4.png";
 function Home() {
   return (
     <>
       <div className="n">
         <div className="m-c">
-          <h1 className="logo">
+          <h1 className="logo" >
             <div className="h-l-c">
               <img src={house} alt="" />{" "}
               <img className="winlogo" src={window} alt="" />
@@ -17,8 +21,10 @@ function Home() {
             <li>Place to stay</li>
             <li>NFTs</li>
             <li>Community</li>
+            <button className="connect-btn in">Connect wallet</button>
           </ul>
-          <button className="connect-btn">Connect wallet</button>
+          <button className="connect-btn out">Connect wallet</button>
+          <button className='menu'>Menu</button>
         </div>
         <section>
           <div className="t-s">
@@ -34,6 +40,16 @@ function Home() {
             <div className="search-container">
               <input type="search" placeholder="Search for location" />
               <button>search</button>
+            </div>
+          </div>
+          <div className="img-section">
+            <div className="img-ss frst">
+              <img src={img1} alt="" />
+              <img src={img2} alt="" />
+            </div>
+            <div className="img-ss snd">
+              <img src={img3} alt="" />
+              <img src={img4} alt="" />
             </div>
           </div>
         </section>
